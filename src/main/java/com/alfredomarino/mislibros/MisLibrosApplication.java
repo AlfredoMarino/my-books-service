2e04285426255406c2758415031dfaeb6bbda492
Alfredo <alfredoalejandro05@gmail.com>
2019-01-20 01:48:23 -0300

    Inicio del proyecto

---
+++ src/main/java/com/alfredomarino/mislibros/MisLibrosApplication.java
@@ -0,0 +1,14 @@
+package com.alfredomarino.mislibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisLibrosApplication.class, args);
+	}
+
+}
+
